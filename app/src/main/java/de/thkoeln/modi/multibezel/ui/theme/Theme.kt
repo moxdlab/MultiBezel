c7c2961c2342010e6bf3643782d57a790a251b06
MReuter2 <marvin122333444455555@gmail.com>
2024-05-26 18:54:57 +0200

    first commit

---
+++ app/src/main/java/de/thkoeln/modi/multibezel/ui/theme/Theme.kt
@@ -0,0 +1,17 @@
+package de.thkoeln.modi.multibezel.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun MultiBezelTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
